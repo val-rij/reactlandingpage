2f7bff34b6cbb8099b35e006023242ab74536a87
carlosiglesias-lab <carlos.igles@gmail.com>
2022-12-07 12:12:07 +0000

    footer

---
+++ src/js/component/footer.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+const Footer = () => {
+    return (
+        <footer class="page-footer font-small blue">
+
+            <div class="footer-copyright text-center py-3">© 2022 Copyright:
+                <a href="/">Val-Carlos Project</a>
+            </div>
+
+        </footer>
+
+
+    )
+}
+export default Footer
